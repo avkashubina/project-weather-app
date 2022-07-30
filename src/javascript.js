068345273918e34707da9efd519ed3958ef1c186
BamBamBaa <Annakashubina@gmail.com>
2022-07-30 20:39:43 +0200

    added main icon js

--- src/javascript.js
+++ src/javascript.js
@@ -2,8 +2,10 @@ function showInfo(response) {
   let city = document.querySelector("#city");
   city.innerHTML = response.data.name;
   console.log(response.data);
+
   let temperature = document.querySelector("#degree");
   temperature.innerHTML = Math.round(response.data.main.temp);
+
   let showDate = document.querySelector("#date");
   let currentDate = new Date();
   let date = currentDate.getDate();
@@ -32,10 +34,18 @@ function showInfo(response) {
 
   let sky = document.querySelector("#sky");
   sky.innerHTML = response.data.weather[0].main;
+
   let humidity = document.querySelector("#humidity");
   humidity.innerHTML = Math.round(response.data.main.humidity);
+
   let wind = document.querySelector("#wind");
   wind.innerHTML = Math.round(response.data.wind.speed);
+
+  let mainIcon = document.querySelector("#icon");
+  mainIcon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`
+  );
 }
 
 let apiKey = "370b3975f0a546c7a8755cf3240ff7fd";
