66b83507e35ebe677f2ebb7abb58daa7d47ee5bf
BamBamBaa <Annakashubina@gmail.com>
2022-08-04 21:04:55 +0200

    started add to js api for forecast

--- src/javascript.js
+++ src/javascript.js
@@ -1,3 +1,9 @@
+function getForecast(coordinates) {
+  apiKey = "370b3975f0a546c7a8755cf3240ff7fd";
+  apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function showInfo(response) {
   let city = document.querySelector("#city");
   city.innerHTML = response.data.name;
@@ -46,6 +52,8 @@ function showInfo(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`
   );
   celsius = Math.round(response.data.main.temp);
+
+  getForecast(response.data.coord);
 }
 
 function searchCity(city) {
@@ -81,7 +89,7 @@ function displayCelsius(event) {
   fahrenheitValue.classList.remove("active");
 }
 
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
@@ -98,7 +106,8 @@ function displayForecast() {
             <li>23˚C</li>
           </ul>
           </div>
-          </div></div>`;
+          </div>
+          </div>`;
   });
 
   forecastHTML = forecastHTML + `</div>`;
@@ -118,4 +127,3 @@ let celsiusValue = document.querySelector("#celsius-value");
 celsiusValue.addEventListener("click", displayCelsius);
 
 searchCity("Kyiv");
-displayForecast();
